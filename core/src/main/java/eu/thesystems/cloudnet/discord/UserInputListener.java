24bafcad4bf0ad813022aab2f8529eacf257e4f7
Optischa <fabian@atomicnetworks.eu>
2020-12-19 23:36:23 +0100

    Fix JDA

--- core/src/main/java/eu/thesystems/cloudnet/discord/UserInputListener.java
+++ core/src/main/java/eu/thesystems/cloudnet/discord/UserInputListener.java
@@ -5,10 +5,10 @@ package eu.thesystems.cloudnet.discord;
 
 import eu.thesystems.cloudnet.discord.utility.MapBuilder;
 import lombok.AllArgsConstructor;
-import net.dv8tion.jda.core.EmbedBuilder;
-import net.dv8tion.jda.core.entities.TextChannel;
-import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
-import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.util.function.Consumer;
 
